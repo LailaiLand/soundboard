9f47412b607adf7ac86e2ff39fe2c8f9775ad902
Nikolaipe <nikolaipe@getacademy.no>
2022-10-28 20:23:03 +0200

    DO NOT PUSH!

--- script.js
+++ script.js
@@ -2,45 +2,286 @@ let lyd1 = document.getElementById('lydfil1')
 let lyd2 = document.getElementById('lydfil2')
 let lyd3 = document.getElementById('lydfil3')
 let lyd4 = document.getElementById('lydfil4')
+let lyd5 = document.getElementById('lydfil5')
+let lyd6 = document.getElementById('lydfil6')
+let lyd7 = document.getElementById('lydfil7')
+let lyd8 = document.getElementById('lydfil8')
+let lyd9 = document.getElementById('lydfil9')
+let lyd10 = document.getElementById('lydfil10')
+let lyd11 = document.getElementById('lydfil11')
+let lyd12 = document.getElementById('lydfil12')
+let lyd13 = document.getElementById('lydfil13')
+let lyd14 = document.getElementById('lydfil14')
+let lyd15 = document.getElementById('lydfil15')
+let lyd16 = document.getElementById('lydfil16')
+let lyd17 = document.getElementById('lydfil17')
+let lyd18 = document.getElementById('lydfil18')
+let lyd19 = document.getElementById('lydfil19')
+let lyd20 = document.getElementById('lydfil20')
+let lyd21 = document.getElementById('lydfil21')
+let lyd22 = document.getElementById('lydfil22')
+let lyd23 = document.getElementById('lydfil23')
+let lyd24 = document.getElementById('lydfil24')
+let lyd25 = document.getElementById('lydfil25')
+let lyd26 = document.getElementById('lydfil26')
+let lyd27 = document.getElementById('lydfil27')
+let lyd28 = document.getElementById('lydfil28')
+let lyd29 = document.getElementById('lydfil29')
+let lyd30 = document.getElementById('lydfil30')
+let lyd31 = document.getElementById('lydfil31')
+let lyd32 = document.getElementById('lydfil32')
+let lyd33 = document.getElementById('lydfil33')
+let lyd34 = document.getElementById('lydfil34')
+let lyd35 = document.getElementById('lydfil35')
+let lyd36 = document.getElementById('lydfil36')
+let lyd37 = document.getElementById('lydfil37')
+let lyd38 = document.getElementById('lydfil38')
+let lyd39 = document.getElementById('lydfil39')
+let lyd40 = document.getElementById('lydfil40')
+let lyd41 = document.getElementById('lydfil41')
+let lyd42 = document.getElementById('lydfil42')
+let lyd43 = document.getElementById('lydfil43')
+let lyd44 = document.getElementById('lydfil44')
+let lyd45 = document.getElementById('lydfil45')
+let lyd46 = document.getElementById('lydfil46')
+let lyd47 = document.getElementById('lydfil47')
+let lyd48 = document.getElementById('lydfil48')
+let lyd49 = document.getElementById('lydfil49')
+let lyd50 = document.getElementById('lydfil50')
+let lyd51 = document.getElementById('lydfil51')
+let lyd52 = document.getElementById('lydfil52')
+let lyd53 = document.getElementById('lydfil53')
+let lyd54 = document.getElementById('lydfil54')
+let lyd55 = document.getElementById('lydfil55')
+let lyd56 = document.getElementById('lydfil56')
+let lyd57 = document.getElementById('lydfil57')
+let lyd58 = document.getElementById('lydfil58')
+let lyd59 = document.getElementById('lydfil59')
+let lyd60 = document.getElementById('lydfil60')
+let lyd61 = document.getElementById('lydfil61')
+let lyd62 = document.getElementById('lydfil62')
+let lyd63 = document.getElementById('lydfil63')
+let lyd64 = document.getElementById('lydfil69')
+let lyd65 = document.getElementById('lydfil64')
+let lyd66 = document.getElementById('lydfil65')
+let lyd67 = document.getElementById('lydfil66')
+let lyd68 = document.getElementById('lydfil67')
+let lyd69 = document.getElementById('lydfil68')
+let lyd70 = document.getElementById('lydfil70')
+let lyd71 = document.getElementById('lydfil71')
+let lyd72 = document.getElementById('lydfil72')
+let lyd73 = document.getElementById('lydfil73')
+let lyd74 = document.getElementById('lydfil74')
+let lyd75 = document.getElementById('lydfil75')
+let lyd76 = document.getElementById('lydfil76')
+let lyd77 = document.getElementById('lydfil77')
+let lyd78 = document.getElementById('lydfil78')
+let lyd79 = document.getElementById('lydfil79')
+let lyd80 = document.getElementById('lydfil80')
+let lyd81 = document.getElementById('lydfil81')
+let lyd82 = document.getElementById('lydfil82')
+let lyd83 = document.getElementById('lydfil83')
+let lyd84 = document.getElementById('lydfil84')
+let lyd85 = document.getElementById('lydfil85')
+let lyd86 = document.getElementById('lydfil86')
+let lyd87 = document.getElementById('lydfil87')
+let lyd88 = document.getElementById('lydfil88')
+let lyd89 = document.getElementById('lydfil89')
+let lyd90 = document.getElementById('lydfil90')
+let lyd91 = document.getElementById('lydfil91')
+let lyd92 = document.getElementById('lydfil92')
+let lyd93 = document.getElementById('lydfil93')
+let lyd94 = document.getElementById('lydfil94')
+let lyd95 = document.getElementById('lydfil95')
+let lyd96 = document.getElementById('lydfil96')
+let lyd97 = document.getElementById('lydfil97')
+let lyd98 = document.getElementById('lydfil98')
+let lyd99 = document.getElementById('lydfil99')
+let lyd100 = document.getElementById('lydfil100')
+let lyd101 = document.getElementById('lydfil101')
+let lyd102 = document.getElementById('lydfil102')
+let lyd103 = document.getElementById('lydfil103')
+let lyd104 = document.getElementById('lydfil104')
+let lyd105 = document.getElementById('lydfil105')
+let lyd106 = document.getElementById('lydfil106')
+let lyd107 = document.getElementById('lydfil107')
+let lyd108 = document.getElementById('lydfil108')
+let lyd109 = document.getElementById('lydfil109')
+let lyd110 = document.getElementById('lydfil110')
+let lyd111 = document.getElementById('lydfil111')
+let lyd112 = document.getElementById('lydfil112')
+let lyd113 = document.getElementById('lydfil113')
+let lyd114 = document.getElementById('lydfil114')
+let lyd115 = document.getElementById('lydfil115')
 
 function lydfilnavn(select){
     if(select == 'loop1'){
         lyd1.innerHTML = /*HTML*/ `
-        <audio src="sounds/mario-boing.WAV" loop autoplay></audio>
-        Boing
+        <audio src="sounds/bird1.WAV" loop autoplay></audio>
+        bird1
         `;
     }
     if(select== 'muted1'){
-        lyd1.innerHTML = 'Boing'
+        lyd1.innerHTML = 'bird1'
     }
-    
+
     if(select == 'loop2'){
         lyd2.innerHTML = /*HTML*/ `
-        <audio src="sounds/mario-buhbye.WAV" loop autoplay></audio>
-        Buhbye
+        <audio src="sounds/bird2.WAV" loop autoplay></audio>
+        bird2
         `;
     }
     if(select== 'muted2'){
-        lyd2.innerHTML = 'Buhbye'
+        lyd2.innerHTML = 'bird2'
     }
-    
+
     if(select == 'loop3'){
         lyd3.innerHTML = /*HTML*/ `
-        <audio src="sounds/mario-dream.WAV" loop autoplay></audio>
-        Dream
+        <audio src="sounds/bird3.WAV" loop autoplay></audio>
+        bird3
         `;
     }
     if(select== 'muted3'){
-        lyd3.innerHTML = 'Dream'
+        lyd3.innerHTML = 'bird3'
     }
     
     if(select == 'loop4'){
         lyd4.innerHTML = /*HTML*/ `
-        <audio src="sounds/mario-fire.WAV" loop autoplay></audio>
-        Fire
+        <audio src="sounds/boing.WAV" loop autoplay></audio>
+        boing
         `;
     }
     if(select== 'muted4'){
-        lyd4.innerHTML = 'Fire'
+        lyd4.innerHTML = 'boing'
+    }
+    
+    if(select == 'loop5'){
+        lyd5.innerHTML = /*HTML*/ `
+        <audio src="sounds/boo.WAV" loop autoplay></audio>
+        boo
+        `;
+    }
+    if(select== 'muted5'){
+        lyd5.innerHTML = 'boo'
+    }
+    
+    if(select == 'loop6'){
+        lyd6.innerHTML = /*HTML*/ `
+        <audio src="sounds/bowser-die.WAV" loop autoplay></audio>
+        bowser-die
+        `;
+    }
+    if(select== 'muted6'){
+        lyd6.innerHTML = 'bowser-die'
+    }
+    
+    if(select == 'loop7'){
+        lyd7.innerHTML = /*HTML*/ `
+        <audio src="sounds/bowser-fire.WAV" loop autoplay></audio>
+        bowser-fire
+        `;
+    }
+    if(select== 'muted7'){
+        lyd7.innerHTML = 'bowser-fire'
+    }
+    
+    if(select == 'loop8'){
+        lyd8.innerHTML = /*HTML*/ `
+        <audio src="sounds/bowser-grab.WAV" loop autoplay></audio>
+        bowser-grab
+        `;
+    }
+    if(select== 'muted8'){
+        lyd8.innerHTML = 'bowser-grab'
+    }
+    
+    if(select == 'loop9'){
+        lyd9.innerHTML = /*HTML*/ `
+        <audio src="sounds/bowser-inhale.WAV" loop autoplay></audio>
+        bowser-inhale
+        `;
+    }
+    if(select== 'muted9'){
+        lyd9.innerHTML = 'bowser-inhale'
+    }
+    
+    if(select == 'loop10'){
+        lyd10.innerHTML = /*HTML*/ `
+        <audio src="sounds/bowser-something.WAV" loop autoplay></audio>
+        bowser-something
+        `;
+    }
+    if(select== 'muted10'){
+        lyd10.innerHTML = 'bowser-something'
+    }
+    
+    if(select == 'loop11'){
+        lyd11.innerHTML = /*HTML*/ `
+        <audio src="sounds/bowser-step.WAV" loop autoplay></audio>
+        bowser-step
+        `;
+    }
+    if(select== 'muted11'){
+        lyd11.innerHTML = 'bowser-step'
+    }
+    
+    if(select == 'loop12'){
+        lyd12.innerHTML = /*HTML*/ `
+        <audio src="sounds/boxbreak.WAV" loop autoplay></audio>
+        boxbreak
+        `;
+    }
+    if(select== 'muted12'){
+        lyd12.innerHTML = 'boxbreak'
+    }
+    
+    if(select == 'loop13'){
+        lyd13.innerHTML = /*HTML*/ `
+        <audio src="sounds/bubble.WAV" loop autoplay></audio>
+        bubble
+        `;
+    }
+    if(select== 'muted13'){
+        lyd13.innerHTML = 'bubble'
+    }
+    
+    if(select == 'loop14'){
+        lyd14.innerHTML = /*HTML*/ `
+        <audio src="sounds/cannon-fire.WAV" loop autoplay></audio>
+        cannon-fire
+        `;
+    }
+    if(select== 'muted14'){
+        lyd14.innerHTML = 'cannon-fire'
+    }
+    
+    if(select == 'loop15'){
+        lyd15.innerHTML = /*HTML*/ `
+        <audio src="sounds/cannon1.WAV" loop autoplay></audio>
+        cannon1
+        `;
+    }
+    if(select== 'muted15'){
+        lyd15.innerHTML = 'cannon1'
+    }
+    
+    if(select == 'loop16'){
+        lyd16.innerHTML = /*HTML*/ `
+        <audio src="sounds/cannon2.WAV" loop autoplay></audio>
+        cannon2
+        `;
+    }
+    if(select== 'muted16'){
+        lyd16.innerHTML = 'cannon2'
+    }
+    
+    if(select == 'loop'){
+        lyd.innerHTML = /*HTML*/ `
+        <audio src="" loop autoplay></audio>
+        
+        `;
+    }
+    if(select== 'muted'){
+        lyd.innerHTML = ''
     }
 }
